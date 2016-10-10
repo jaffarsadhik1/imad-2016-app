1e7d40f51ad03032b53b8db77086ca090025e47e
jaffarsadhik1 <jaffarsadhika@psnacet.edu.in>
2016-10-10 16:36:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ var image =document.getElementById('madi');
  var marginLeft = 0;
 function moveRight()
 {
-    marginLeft =  marginLeft + 5;
+    marginLeft =  marginLeft + 1;
     madi.style.marginLeft = marginLeft + 'px';
 }
 madi.onclick=function(){
