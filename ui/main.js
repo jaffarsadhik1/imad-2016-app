4a7f97ec884c660c55b6b588dd29e221a5a4ad74
jaffarsadhik1 <jaffarsadhika@psnacet.edu.in>
2016-10-10 16:19:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,4 +6,8 @@ var element=document.getElementById('main-text')
 
 element.innerHTML='new value';
 
-
+//code to move image
+var image =document.getElementById('img');
+img.onclick=function(){
+   img.style.marginLeft='100px' 
+};
