b7c24fa02238e06d43d1e6d04150b1d3c56be4b5
jaffarsadhik1 <jaffarsadhika@psnacet.edu.in>
2016-10-12 20:42:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,23 @@
 //code for counter button
 var button = document.getElementById('counter');
-var counter =0;
 button.onclick = function () {
   
-    //make a request to counter
-    
+    //create a request
+    var request = new XMLHttpRequest();
     //capture the request and store it as avariable
+    request,onreadystatechange = function(){
+      if(request.readystate === XMLHttpRequest.DONE){
+          if(request.status=== 200)
+          {
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML=counter.toString();
     
-    //render the variable in correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
+          }
+      }  
+    };
+  //make a request
+  request.open('GET','http://jaffarsadhik1.imad.hasura-app.io/counter', true);
+  request.send(null);
+  
 };
