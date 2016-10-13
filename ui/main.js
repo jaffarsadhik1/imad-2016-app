6b9dcc974cb80cc0e7023c363d063e97732efb5e
jaffarsadhik1 <jaffarsadhika@psnacet.edu.in>
2016-10-13 22:20:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function () {
   
 };
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
 {
@@ -54,6 +53,8 @@ submit.onclick = function()
     //not done yet
     };
   //make a request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET','http://jaffarsadhik1.imad.hasura-app.io/submit-name?name =' + name, true);
   request.send(null);
  
